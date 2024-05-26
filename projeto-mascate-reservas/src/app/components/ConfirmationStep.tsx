7ae6347461bac12871462fb38e184c691c1a0cdf
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-05-26 12:51:43 -0300

    fixed employeeID | add library uuid and react-input-mask

--- projeto-mascate-reservas/src/app/components/ConfirmationStep.tsx
+++ projeto-mascate-reservas/src/app/components/ConfirmationStep.tsx
@@ -7,38 +7,40 @@ interface ConfirmationStepProps {
   selectedTable: { type: string; number: number; numChairs: number } | null;
   onPreviousStep: () => void;
   onSubmit: () => void;
+  customerName: string;  // Adicionado customerName
+  employeeId: string;    // Adicionado employeeId
 }
 
-
-
-interface FormData {
-  employeeId: string;
-}
-
-const ConfirmationStep: React.FC<ConfirmationStepProps> = ({ selectedDate, selectedTime, selectedTable, onPreviousStep, onSubmit }) => {
-  const { register, handleSubmit } = useForm<FormData>();
-
+const ConfirmationStep: React.FC<ConfirmationStepProps> = ({
+  selectedDate,
+  selectedTime,
+  selectedTable,
+  onPreviousStep,
+  onSubmit,
+  customerName,
+  employeeId
+}) => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md">
       <h2 className="text-xl font-bold mb-4">Confirmação da Reserva</h2>
       <p>Data da Reserva: {selectedDate.toISOString().split('T')[0]}</p>
       <p>Horário da Reserva: {selectedTime}</p>
       {selectedTable && (
-  <p>
-    Mesa Número: {selectedTable.number} ({selectedTable.type.replace('_', ' ')})
-    <br />
-    Número de Cadeiras: {selectedTable.numChairs}
-  </p>
-)}
-      <div className="mt-4">
-        <label className="block text-gray-700">Número de Identificação do Funcionário:</label>
-        <input {...register('employeeId')} type="text" className="input-field" />
-      </div>
+        <p>
+          Nome do cliente: {customerName}
+          <br />
+          Mesa Número: {selectedTable.number} ({selectedTable.type.replace('_', ' ')})
+          <br />
+          Número de Cadeiras: {selectedTable.numChairs}
+          <br />
+          ID do funcionário: {employeeId}
+        </p>
+      )}
       <div className="mt-4 flex justify-between">
         <button type="button" onClick={onPreviousStep} className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300">
           Anterior
         </button>
-        <button type="submit" onClick={handleSubmit(onSubmit)} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300">
+        <button type="submit" onClick={onSubmit} className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300">
           Realizar Reserva
         </button>
       </div>
--- projeto-mascate-reservas/src/app/components/ReservationForm.tsx
+++ projeto-mascate-reservas/src/app/components/ReservationForm.tsx
@@ -3,6 +3,9 @@ import { useForm } from 'react-hook-form';
 import CalendarComponent from './CalendarComponent';
 import TableLayout from './TableLayout';
 import ConfirmationStep from './ConfirmationStep';
+import Swal from 'sweetalert2';
+import { v4 as uuidv4 } from 'uuid';
+import InputMask from 'react-input-mask';
 
 interface FormData {
   name: string;
@@ -10,20 +13,29 @@ interface FormData {
   phoneNumber: string;
   date: Date;
   time: string;
-  table: { type: string; number: number, numChairs: number } | null; // Adicionado numChairs
+  table: { type: string; number: number; numChairs: number } | null;
+  employeeId: string;
+  reservationId: string;
 }
 
 const ReservationForm: React.FC = () => {
-  const { register, handleSubmit, setValue } = useForm<FormData>();
+  const { register, handleSubmit, setValue, watch } = useForm<FormData>();
   const [currentStep, setCurrentStep] = useState<number>(1);
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [selectedTime, setSelectedTime] = useState<string>('');
-  const [selectedTable, setSelectedTable] = useState<{ type: string; number: number, numChairs: number } | null>(null); // Estado para armazenar a mesa selecionada
+  const [selectedTable, setSelectedTable] = useState<{ type: string; number: number; numChairs: number } | null>(null);
 
-  const onSubmit = (data: FormData) => {
+  const onSubmit = async (data: FormData) => {
+    const reservationId = uuidv4(); // Gera um ID único para a reserva
+    data.reservationId = reservationId;
     console.log(data);
-    // Aqui você pode fazer uma requisição para o backend para salvar a reserva
-    setCurrentStep(4); // Avançar para a etapa de confirmação
+    Swal.fire({
+      title: 'Reserva Realizada',
+      text: `Reserva para a mesa ${selectedTable?.number} com ${selectedTable?.numChairs} cadeiras no dia ${selectedDate?.toISOString().split('T')[0]} às ${selectedTime} foi realizada com sucesso. ID da reserva: ${reservationId}`,
+      icon: 'success',
+      confirmButtonText: 'OK'
+    });
+    setCurrentStep(4);
   };
 
   const handleNextStep = () => {
@@ -38,10 +50,13 @@ const ReservationForm: React.FC = () => {
     setSelectedTime(time);
   };
 
-  const handleTableSelect = (table: { type: string; number: number, numChairs: number }) => { // Adicionado numChairs
+  const handleTableSelect = (table: { type: string; number: number; numChairs: number }) => {
     setValue('table', table);
     setSelectedTable(table);
   };
+
+  const customerName = watch('name');
+  const employeeId = watch('employeeId');
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="max-w-lg mx-auto p-8 bg-white shadow-md rounded-lg">
@@ -54,11 +69,23 @@ const ReservationForm: React.FC = () => {
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">CPF:</label>
-            <input {...register('cpf')} type="text" className="input-field" />
+            <InputMask
+              {...register('cpf')}
+              mask="999.999.999-99"
+              className="input-field"
+            />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">Número de Telefone:</label>
-            <input {...register('phoneNumber')} type="text" className="input-field" />
+            <InputMask
+              {...register('phoneNumber')}
+              mask="(99) 9 9999-9999"
+              className="input-field"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">ID do Funcionário:</label>
+            <input {...register('employeeId')} type="text" className="input-field" />
           </div>
           <div className="mt-4 flex justify-between">
             <button>
@@ -93,17 +120,16 @@ const ReservationForm: React.FC = () => {
               date={selectedDate}
               time={selectedTime}
               onTableSelect={handleTableSelect}
-              selectedTable={selectedTable} // Passa a mesa selecionada para TableLayout
+              selectedTable={selectedTable}
             />
           )}
           <div className="mt-4 flex justify-between">
             <button type="button" onClick={handlePreviousStep} className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300">
               Anterior
             </button>
-            <button type="button" onClick={handleNextStep}className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
+            <button type="button" onClick={handleNextStep} className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
               Próximo
             </button>
-            
           </div>
         </div>
       )}
@@ -111,9 +137,11 @@ const ReservationForm: React.FC = () => {
         <ConfirmationStep
           selectedDate={selectedDate}
           selectedTime={selectedTime}
+          selectedTable={selectedTable}
           onPreviousStep={handlePreviousStep}
           onSubmit={handleSubmit(onSubmit)}
-          selectedTable={selectedTable}
+          customerName={customerName}
+          employeeId={employeeId}
         />
       )}
     </form>
@@ -121,4 +149,3 @@ const ReservationForm: React.FC = () => {
 };
 
 export default ReservationForm;
-
