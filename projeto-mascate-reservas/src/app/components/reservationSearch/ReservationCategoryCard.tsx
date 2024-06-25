e7422f9fd9788ca7c33722f8d1028b18815bc2db
Miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-06-25 20:03:50 -0300

    bug fix
    image object fit

--- projeto-mascate-reservas/src/app/components/reservationSearch/ReservationCategoryCard.tsx
+++ projeto-mascate-reservas/src/app/components/reservationSearch/ReservationCategoryCard.tsx
@@ -11,8 +11,8 @@ interface ReservationCategoryCardProps {
 
 const ReservationCategoryCard: React.FC<ReservationCategoryCardProps> = ({ imageSrc, title, description, onClick }) => {
   return (
-    <div onClick={onClick} className="cursor-pointer border border-gray-300 rounded-lg p-4 hover:shadow-lg transition duration-300">
-      <Image src={imageSrc} alt={title} width={300} height={200} className="w-full h-32 object-cover rounded-t-lg" />
+    <div onClick={onClick} className="cursor-pointer border border-gray-300 rounded-lg  p-4 hover:shadow-lg transition duration-300">
+      <Image src={imageSrc} alt={title} width={300} height={200} className="w-full h-32 object-fit rounded-t-lg" />
       <h3 className="text-xl font-semibold mt-2">{title}</h3>
       <p className="text-gray-500">{description}</p>
     </div>
